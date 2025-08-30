04eb7bc30c9e168639eb009fe1969f612312dc5f
nghuraiya <nehaghuraiya.info@gmail.com>
2025-08-30 18:29:11 +0530

    Fix import issue in ProductContext

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { productData } from '../data/productData';
+import { products as productData } from '../data/productData';
 
 const ProductContext = createContext();
 
